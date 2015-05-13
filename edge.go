e4f8d93aafdd366608e31bf32ec0d3c4e69e41a7
Jens Fredskov <jfrdev@gmail.com>
2015-05-13 13:02:50 +0200

    Finished the topology creation. Testing still needed

--- edge.go
+++ edge.go
@@ -1,28 +1,23 @@
 package main
 
-import (
-	"math"
-)
-
 // An edge is defined as the index of the two points defining is begin and end
 // points in a tree. The edge should always follow the convention that P1 < P2.
 type Edge struct {
+	P0 int
 	P1 int
-	P2 int
-	T  *Tree
 }
 
-func (e *Edge) Length() float64 {
-	// Remeber that we 0-index the slices, but that point indexes start a 1!
-	p1 := e.T.Points[e.P1-1]
-	p2 := e.T.Points[e.P2-1]
-	AssertDim(p1, p2)
+// func (e *Edge) Length() float64 {
+// 	// Remeber that we 0-index the slices, but that point indexes start a 1!
+// 	p1 := e.T.Points[e.P1-1]
+// 	p2 := e.T.Points[e.P2-1]
+// 	AssertDim(p1, p2)
 
-	dist := 0.0
-	for i := range p1 {
-		diff := p1[i] - p2[i]
-		dist = dist + (diff * diff)
-	}
-	dist = math.Sqrt(dist)
-	return dist
-}
+// 	dist := 0.0
+// 	for i := range p1 {
+// 		diff := p1[i] - p2[i]
+// 		dist = dist + (diff * diff)
+// 	}
+// 	dist = math.Sqrt(dist)
+// 	return dist
+// }
--- tree.go
+++ tree.go
@@ -1,5 +1,7 @@
 package main
 
+import "math/rand"
+
 // A tree. This contains all N regular points, as the first N entries, and all
 // N-2 Steiner points, as the N+1..2N-2 entries. The topology is then defined by
 // the edges.
@@ -19,44 +21,77 @@ func (t *Tree) Init(points []Point) {
 		panic("Too few points to initialize.")
 	}
 
-	s0 := GetPertubedCentroid(t.Points[0], t.Points[1], t.Points[2])
+	s := t.GetPertubedCentroid(0, 1, 2)
 
-	e0 := Edge{1, n + 1, t}
-	e1 := Edge{2, n + 1, t}
-	e2 := Edge{3, n + 1, t}
+	e0 := Edge{0, n + 1}
+	e1 := Edge{1, n + 1}
+	e2 := Edge{2, n + 1}
 
 	// there are n+k-1 edges = n+(n-2)-1 = 2n-3
 	edges := make([]Edge, 3, 2*t.N-3)
 	edges = []Edge{e0, e1, e2}
 
 	t.N = n
-	t.Points = append(t.Points, s0)
+	t.Points = append(t.Points, s)
 	t.Edges = edges
 }
 
-func (t *Tree) Sprout(edgeIdx, pointIdx) {
-	edgeNo := len(t.Edges)
-	steinerNo := len(t.Edges)
+func (t *Tree) Sprout(edgeIdx, p2 int) {
 
 	// Select the edge we split. Use a pointer for easy editing
-	sproutEdge := &t.Edges[edgeIdx]
+	e0 := &t.Edges[edgeIdx]
 
-	s := GetPertubedCentroid()
+	// Select the terminals
+	p0 := e0.P0
+	p1 := e0.P1
 
-	newEdge1 := Edge{}
+	// Get the new Steiner point and its number, and append to point list
+	s := t.GetPertubedCentroid(p0, p1, p2)
+	sIdx := len(t.Points)
+	t.Points = append(t.Points, s)
+
+	// Create the new edges and append them to the edge list
+	e1 := Edge{p0, sIdx}
+	e2 := Edge{p1, sIdx}
+	t.Edges = append(t.Edges, e1)
+	t.Edges = append(t.Edges, e2)
+
+	// Change the end points of the original edge
+	e0.P0 = p2
+	e0.P1 = sIdx
 }
 
-// func (t *Tree) Sprout(edgeIdx int, pointIdx int) {
-// 	// Find out the current edge number
-// 	//	edgeNo := len(t.Edges)
+func (t *Tree) Restore(edgeIdx int) {
+	idx := len(t.Edges)
 
-// 	// Select the edge we sprout on
-// 	//	sproutEdge := &t.Edges[edgeIdx]
+	// Get the edges we need to remove (e0, e1)
+	// and the edge we need to restore (e2)
+	e0 := t.Edges[idx-1]
+	e1 := t.Edges[idx-2]
+	e2 := &t.Edges[edgeIdx]
 
-// 	// newEdge1 := Edge{[]int{}, 0.0}
-// 	// newEdge2 := Edge{[]int{}, 0.0}
-// }
+	if e0.P1 != e1.P1 || e1.P1 != e2.P1 || e2.P1 != e0.P1 {
+		panic("The edges does not go to the same Steiner point")
+	}
 
-// func (t *Tree) Restore() {
-// 	// Removes a RPoint and any extra SPoints created
-// }
+	// Restore edge
+	e2.P0 = e0.P0
+	e2.P1 = e1.P0
+
+	// Remove Steiner point and last two edges
+	t.Points = t.Points[:len(t.Points)-1]
+	t.Edges = t.Edges[:idx-2]
+
+}
+
+func (t *Tree) GetPertubedCentroid(idx0, idx1, idx2 int) Point {
+	p0 := t.Points[idx0]
+	p1 := t.Points[idx1]
+	p2 := t.Points[idx2]
+	p := make(Point, len(p0))
+	for i := range p {
+		p[i] = (p0[i]+p1[i]+p2[i])/3.0 +
+			0.001*rand.Float64()
+	}
+	return p
+}
